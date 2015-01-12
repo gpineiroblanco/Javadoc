d6c86e80c23dfc34257fc5234dead2c5b85dd9e6
gpineiroblanco <gpineiroblanco@eniac18.danielcastelao.org>
2015-01-12 10:08:53 +0100

    Commit tonteria

---
+++ src/clase_java/Clase_Java.java
@@ -0,0 +1,17 @@
+
+package clase_java;
+
+/**
+ *
+ * @author gpineiroblanco
+ */
+public class Clase_Java {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+    
+}
